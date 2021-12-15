cf017636cd2fd742416d98a5386d18893e6d692f
BaderDak0 <baderdaka28@gmail.com>
2021-12-15 11:40:07 +0200

    DB models Done+Routers+Controllers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GreenPoints-backend
+steps:
+1) DB models Done+Routers+Controllers
--- models/items.js
+++ models/items.js
@@ -1,5 +1,4 @@
 const { Schema, model } = require('mongoose');
-
 const itemsSchema = new Schema({
     type: { type: String, required: true },
     size: { type: Number, required: true },
