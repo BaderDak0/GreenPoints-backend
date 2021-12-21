55ef74cdf4e5d629bb83f70ff0d57f35d4dfc5d1
BaderDak0 <baderdaka28@gmail.com>
2021-12-21 10:08:15 +0200

    deploy to heroku+cors fix

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const { ItemsRouter } = require("./routers/itemsRouter");
 const { RecycleBinsRouter } = require("./routers/recycleBinsRouter");
 const { UsersRouter } = require("./routers/usersRouter");
 const { infologger, errorlogger } = require("./logs/logs");
+const corsConfig = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    next()
+}
 
+app.use(corsConfig);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', UsersRouter);
