e07da4775fbf2e7c80f9b477330b020b0da8d2a3
BaderDak0 <baderdaka28@gmail.com>
2021-12-21 15:25:05 +0200

    payload limit

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const corsConfig = function (req, res, next) {
     next()
 }
 app.use(corsConfig);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: true ,limit: '50mb'}));
 // app.use(bodyParser({limit: '50mb'}))
 app.use('/api/users', UsersRouter);
 app.use('/api/recycleBins', RecycleBinsRouter);
