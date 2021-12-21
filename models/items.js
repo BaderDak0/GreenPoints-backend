6fb586caac1697d1801cd613e422a1bb38be9f42
BaderDak0 <baderdaka28@gmail.com>
2021-12-21 12:04:36 +0200

    controllers fix

--- models/items.js
+++ models/items.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 const itemsSchema = new Schema({
     type: { type: String, required: true },
+    name: { type: String, required: true },
     size: { type: Number, required: true },
 }, { collection: 'items' });
 const Item = model('Item', itemsSchema);
