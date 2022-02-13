f7376a77a86676d34fdabf339bdad5d9cf97490b
Noor Haj Dawood <noorhajdawood@gmail.com>
2022-02-13 22:25:53 +0200

    cors fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const corsConfig = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE');
-    res.header('Access-Control-Allow-Headers', 'x-access-token', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 }
 app.use(corsConfig);
