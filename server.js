4814a3e788d74b94b2ddd8e6a4dc01ebb4e16f44
Noor Haj Dawood <noorhajdawood@gmail.com>
2022-02-13 20:44:43 +0200

    added missing route

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const app = express();
 const port = process.env.PORT || 8000;
 const { recycleBinsRouter } = require("./routers/recycleBinsRouter");
 const { usersRouter } = require("./routers/usersRouter");
+const { couponsRouter } = require("./routers/couponsRouter");
 const { authRouter } = require("./routers/authRouter");
 const { infoLogger, errorLogger } = require("./logs/logs");
 const authJwt = require('./middlewares/authJwt');
 
 const corsConfig = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Credentials', true)
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE')
-    res.header('Access-Control-Allow-Headers', 'x-access-token', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    next()
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'x-access-token', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
 }
 app.use(corsConfig);
 
@@ -25,6 +26,7 @@ app.use(authJwt.verifyToken);
 
 app.use('/api/users', usersRouter);
 app.use('/api/recycleBins', recycleBinsRouter);
+app.use('/api/coupons', couponsRouter);
 
 app.use((req, res) => {
     errorLogger.error(`Bad Method Request!:${req.method} ${req.url}`);
