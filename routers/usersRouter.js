d0d447d368adc9ec02f3c076e2e6c6d3f1a72ba7
Noor Haj Dawood <noorhajdawood@gmail.com>
2022-02-14 02:10:37 +0200

    typo fixes

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -4,6 +4,6 @@ const usersRouter = new Router();
 
 usersRouter.get('/', usersController.getUsers); 
 usersRouter.get('/:id', usersController.getUserDetails);  
-usersRouter.post('/:id/activites', usersController.addActivity);
+usersRouter.post('/:id/activities', usersController.addActivity);
 
 module.exports = { usersRouter };
